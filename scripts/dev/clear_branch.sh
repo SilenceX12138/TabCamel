28b566bf51c55c60ab0e8380f23b92e783a4ecc2
silencejiang@cl <silencejiang12138@gmail.com>
2025-08-08 16:16:15 +0100

    Add functionality to delete remote branches from the public repository

--- scripts/dev/clear_branch.sh
+++ scripts/dev/clear_branch.sh
@@ -3,3 +3,4 @@ git branch | grep -v "master" | xargs git branch -D
 # Remove all remote branches except master
 git fetch --prune
 git branch -r | grep -v " -> " | grep "origin/" | grep -v "origin/master$" | sed 's/^\s*origin\///' | xargs -I {} git push origin --delete {}
+git branch -r | grep -v " -> " | grep "public/" | grep -v "public/master$" | sed 's/^\s*public\///' | xargs -I {} git push public --delete {}
